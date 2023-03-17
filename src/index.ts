aa553e30906c59793160e23fd24e52f2ca1030b0
Marcelino Teixeira <marcelino.teixeira.dev@gmail.com>
2023-03-17 10:52:43 -0300

    fix: cors error local and prod

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ app.use(cors())
 
 const server = http.createServer(app)
 
-const io = new Server(server, { cors: { origin: '*' } })
+const io = new Server(server, {
+  cors: { origin: ['http://localhost:5173', 'https://pingado.vercel.app'] },
+})
 
 game(io)
 
